c50589c15721f95dcf81271bfaa4663ee1a8a1bc
Maksim Lyapkin <lyapkin992@gmail.com>
2020-06-09 11:05:08 +0500

    Built menu and created models for it

--- orders/models.py
+++ orders/models.py
@@ -1,2 +1,28 @@
 from django.db import models
 
+
+class Menu(models.Model):
+	item = models.ForeignKey('Item', on_delete=models.CASCADE)
+	section = models.ForeignKey('Section', on_delete=models.CASCADE)
+	price = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+	large_price = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+
+	class Meta:
+		verbose_name_plural = 'menu'
+
+	def __str__(self):
+		return f'{self.section} - {self.item}'
+
+
+class Item(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return f'{self.name}'
+
+
+class Section(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return f'{self.name}'
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Menu
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, 'orders/base.html')
+
+
+def menu(request):
+	menu = {
+		'regular_pizza': Menu.objects.filter(section__name__contains='Regular pizza'),
+		'sicilian_pizza': Menu.objects.filter(section__name__contains='Sicilian pizza'),
+		'toppings': Menu.objects.filter(section__name__contains='Toppings'),
+		'subs': Menu.objects.filter(section__name__contains='Subs'),
+		'pasta': Menu.objects.filter(section__name__contains='Pasta'),
+		'salads': Menu.objects.filter(section__name__contains='Salads'),
+		'dinner_platters': Menu.objects.filter(section__name__contains='Dinner platters')
+	}
+
+	return render(request, 'orders/menu.html', {'menu': menu})
